7983ea711d7839c944c91705ff0f1975818b30e6
HariniV02 <vharinirao@gmail.com>
2024-09-10 22:50:46 -0400

    Inital commit with project files

---
+++ calculator/__init__.py
@@ -0,0 +1,9 @@
+'''This module provides basic arithmetic functions'''
+
+def add(a,b):
+    '''returns the sum'''
+    return a + b
+
+def subtract(a,b):
+    '''returns the difference'''
+    return a - b
