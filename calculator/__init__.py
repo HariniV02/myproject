9779c420dfe895bda01ce44ba278912dd2d7807b
HariniV02 <vharinirao@gmail.com>
2024-09-17 09:40:33 -0400

    removed comments

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,9 +1,5 @@
-'''This module provides basic arithmetic functions'''
-
 def add(a,b):
-    '''returns the sum'''
     return a + b
 
 def subtract(a,b):
-    '''returns the difference'''
     return a - b
