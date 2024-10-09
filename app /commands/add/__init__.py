70f21c9ab7f1fdc9bfd186a21826b75974f28909
HariniV02 <vharinirao@gmail.com>
2024-10-09 09:10:56 -0400

    errors with app folder - remade one

--- app /commands/add/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.commands import Command
-
-class AddCommand(Command):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def execute(self):
-        return self.a + self.b
